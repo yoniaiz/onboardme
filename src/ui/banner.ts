558a468b1f2152d627e4d7038e7e7b465b557da6
yoniaiz <yoni29396.gmail.com>
2026-02-03 08:19:30 +0200

    fix figlet

--- src/ui/banner.ts
+++ src/ui/banner.ts
@@ -1,12 +1,10 @@
+import figlet from "figlet";
+import small from "figlet/fonts/Small";
 import { colors } from "./theme.ts";
 
-const BANNER = `
-   ___       _                      _ __  __
-  / _ \\ _ _ | |__  ___  __ _ _ _ __| |  \\/  |___
- | (_) | ' \\| '_ \\/ _ \\/ _\` | '_/ _\` | |\\/| / -_)
-  \\___/|_||_|_.__/\\___/\\__,_|_| \\__,_|_|  |_\\___|
-`;
+figlet.parseFont("Small", small);
 
 export function printBanner(): void {
-	console.log(colors.primary(BANNER));
+	const banner = figlet.textSync("OnboardMe", { font: "Small" });
+	console.log(colors.primary(banner));
 }
